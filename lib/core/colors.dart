a5f1e7c14095e96bf66bc0cc8d3c778d5339290b
Madhur <30585596+madhurchouhan7@users.noreply.github.com>
2026-01-17 11:17:34 +0530

    feat: Add foundational files for routing, colors, and utilities

---
+++ lib/core/colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+// TODO: Add  theme colors
+// ---------Light Theme Colors---------
+const Color brandColor = Color(0xFF123456);
+const Color lightBackgroundColor = Color(0xFFF5F5F5);
+const Color lightSecondaryColor = Color(0xFF121212);
+
+// ---------Dark Theme Colors----------
+const Color darkBackgroundColor = Color(0xFF121212);
+const Color darkSecondaryColor = Color(0xFFE0E0E0);
+
